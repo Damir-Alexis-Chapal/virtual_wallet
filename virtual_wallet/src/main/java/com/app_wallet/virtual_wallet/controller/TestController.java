fe7ba899eae4c969d334434c4637a17f0289f9bd
Cristhian Osorio <osoriocristhian86@gmail.com>
2025-04-30 10:27:34 -0500

    .

--- virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/controller/TestController.java
+++ virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @GetMapping("/say")
     public String sayHi(){
-        return "index";
+        return "test";
     }
 
 }
