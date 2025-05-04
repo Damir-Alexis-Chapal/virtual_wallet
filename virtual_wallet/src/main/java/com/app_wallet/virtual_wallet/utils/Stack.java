93638ce5f95c48be63ed0fedc0d1ee585ef58555
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2025-05-04 10:44:24 -0500

    pila

---
+++ virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/utils/Stack.java
@@ -0,0 +1,79 @@
+package com.app_wallet.virtual_wallet.utils;
+
+public class Stack<T> {
+    private Node<T> peak;
+    private int size;
+
+    public Stack() {
+        super();
+        peak = null;
+        size = 0;
+    }
+
+    public Node<T> getPeak() {
+        return peak;
+    }
+
+    public void setPeak(Node<T> peak) {
+        this.peak = peak;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void push(T data) {
+        Node<T> aux = new Node<>(data);
+        aux.setNext(peak);
+        peak = aux;
+        size++;
+    }
+
+    public T peek(){
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        return peak.getData();
+    }
+
+    public T pop(){
+        if (isEmpty()) {
+            throw new RuntimeException("La pila esta vacia");
+        }
+        T aux;
+        aux = peak.getData();
+        peak = peak.getNext();
+        size--;
+        return aux;
+    }
+
+    public void print(){
+        if (isEmpty()){
+            System.out.println("La lista esta vacia");
+        }else {
+            Node aux = peak;
+            boolean fin = false;
+            while (!fin) {
+                System.out.println(aux);
+                aux = aux.getNext();
+                if (aux == null){
+                    fin = true;
+                }
+            }
+        }
+    }
+
+    public void clean(){
+        peak = null;
+        size = 0;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+}
