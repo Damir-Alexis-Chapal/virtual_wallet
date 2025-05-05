3937d9d31567a524fbad3c371beb68a2687c50a9
Damir-Alexis-Chapal <dchapalimbaquin2018@gmail.com>
2025-05-05 18:20:46 -0500

    Some classes from the service package were modificated

--- virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/service/PurseService.java
+++ virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/service/PurseService.java
@@ -1,4 +1,23 @@
 package com.app_wallet.virtual_wallet.service;
 
+import com.app_wallet.virtual_wallet.dto.PurseDTO;
+import com.app_wallet.virtual_wallet.dto.TransactionDTO;
+
 public class PurseService {
+    public void createPurse(PurseDTO purse){
+    }
+    public void deletePurse(String id){
+    }
+    public void updatePurse(PurseDTO purse){
+    }
+    public void addMoneyToPurse(Float amount, Long purseId){
+    }
+    public void getMoneyFromPurse(Float amount, Long purseId){
+    }
+    public Long getPurseIdByName(String name){
+        return null;
+    }
+
+
+
 }
--- virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/service/TransactionService.java
+++ virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/service/TransactionService.java
@@ -1,4 +1,12 @@
 package com.app_wallet.virtual_wallet.service;
 
+import com.app_wallet.virtual_wallet.dto.TransactionDTO;
+import com.app_wallet.virtual_wallet.entity.Transaction;
+
 public class TransactionService {
+    public void createTransaction(TransactionDTO transactionDTO){
+    }
+    public void saveTransaction(TransactionDTO transactionDTO){
+    }
+
 }
