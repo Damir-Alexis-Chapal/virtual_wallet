8cef15490190640c69a1a41999a735ae1b030d3a
Cristhian Osorio <osoriocristhian86@gmail.com>
2025-05-20 23:41:23 -0500

    maldito git

--- virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/service/WalletGraphService.java
+++ virtual_wallet/src/main/java/com/app_wallet/virtual_wallet/service/WalletGraphService.java
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 // src/main/java/com/app_wallet/virtual_wallet/service/WalletGraphService.java
-=======
->>>>>>> 651c9d0 (Wallets maso, ME FALTA REVISARLO BN TENGO SUEÑO XD)
 package com.app_wallet.virtual_wallet.service;
 
 import com.app_wallet.virtual_wallet.entity.WalletConnectionEntity;
 import com.app_wallet.virtual_wallet.repository.WalletConnectionRepository;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
 import org.springframework.transaction.annotation.Transactional;
-=======
->>>>>>> 651c9d0 (Wallets maso, ME FALTA REVISARLO BN TENGO SUEÑO XD)
 
 import java.util.*;
 
@@ -47,7 +41,6 @@ public class WalletGraphService {
     public Map<Long, List<Long>> getAdjacencyMap() {
         return Collections.unmodifiableMap(adjacencyMap);
     }
-<<<<<<< HEAD
 
     @Transactional
     public void addConnection(Long userId, Long sourceWalletId, Long targetWalletId) {
@@ -58,6 +51,4 @@ public class WalletGraphService {
         repository.save(e);
         buildGraph(userId);
     }
-=======
->>>>>>> 651c9d0 (Wallets maso, ME FALTA REVISARLO BN TENGO SUEÑO XD)
 }
